7e3ac1e9d3ef4df9976a3d4fe86c8c384f6a1960
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-08-13 11:59:05 -0300

    feat: implement validadte form.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,15 +13,61 @@ import {
 import { Header } from '../components/Header/Header';
 import { BsPlusCircle } from 'react-icons/bs';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import * as zod from 'zod';
+import { v4 as uuidv4 } from 'uuid';
+
 import emptyImg from '../assets/EmptyImg.svg';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type NewTask = {
+  id: string;
+  task?: string;
+  isComplete: boolean;
+};
+
+const newTaskFormSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+});
+
+type NewTaskFormData = zod.infer<typeof newTaskFormSchema>;
 
 export function Home() {
+  const [tasks, setTasks] = useState<NewTask[]>([]);
+
+  const newTaskForm = useForm<NewTaskFormData>({
+    resolver: zodResolver(newTaskFormSchema),
+    defaultValues: {
+      task: '',
+    },
+  });
+
+  const { handleSubmit, register, watch } = newTaskForm;
+
+  function handleCreateNewTask(data: NewTaskFormData) {
+    const newTask = {
+      id: uuidv4(),
+      title: data.task,
+      isComplete: false,
+    };
+
+    setTasks((state) => [...state, newTask]);
+  }
+
+  const task = watch('task');
+  const isSubmitDisable = !task;
+
   return (
     <Center flexDirection="column">
       <Header />
 
       <Stack width="min(90vw, 46rem)" margin="-1.75rem 2rem" spacing={16}>
-        <HStack height="54px">
+        <HStack
+          as="form"
+          height="54px"
+          onSubmit={handleSubmit(handleCreateNewTask)}>
           <Input
             variant="unstyled"
             flex="1"
@@ -29,13 +75,16 @@ export function Home() {
             padding={4}
             placeholder="Adicione uma nova tarefa"
             _placeholder={{ color: 'gray.300' }}
+            {...register('task')}
           />
           <Button
+            type="submit"
             colorScheme="blue"
             size="lg"
             gap={2}
             padding={4}
-            height="100%">
+            height="100%"
+            disabled={isSubmitDisable}>
             Criar
             <Icon as={BsPlusCircle} fontSize={20} />
           </Button>
